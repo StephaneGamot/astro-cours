2cc6ceb6134b0b3c569a6238049aff7ec180dff2
Stéphane Gamot <stephanegamot@outlook.com>
2026-02-07 21:50:18 +0100

    ajout article

--- content/blog/posts.ts
+++ content/blog/posts.ts
@@ -4,11 +4,13 @@ import ThemeAstralPost, { meta as themeAstralMeta } from "./qu-est-ce-qu-un-them
 import ConjonctionPost, { meta as conjonctionMeta } from "./conjonction-melange-des-forces";
 import VenusEnSigne, { meta as venusEnSigne } from "./venus-en-signe-ton-style-amoureux";
 import MarsEnSigne, { meta as marsEnSigne } from "./mars-en-signes-desir-libido-action-sexe";
+import LuneCyclesPost, { meta as luneCyclesMeta } from "./pleine-lune-nouvelle-lune-cycles-astrologie";
 
 export type PostMeta = typeof themeAstralMeta;
 export type PostModule = { meta: PostMeta; Component: () => ReactElement };
 
 export const posts: PostModule[] = [
+  { meta: luneCyclesMeta, Component: LuneCyclesPost },
   { meta: venusEnSigne, Component: VenusEnSigne },
   { meta: marsEnSigne, Component: MarsEnSigne },
   { meta: themeAstralMeta, Component: ThemeAstralPost },
