1673b29994e2d26ba69b3b9dd838d21b8f560121
Stéphane Gamot <stephanegamot@outlook.com>
2026-04-15 22:07:52 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -16,8 +16,9 @@ const nextConfig = {
   // ✅ Image optimization — limit generated sizes for faster builds + smaller downloads
   images: {
     formats: ["image/avif", "image/webp"],
-    deviceSizes: [640, 828, 1080, 1200, 1920],
+    deviceSizes: [640, 750,828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 192, 256, 320, 384],
+    qualities: [40, 50, 60, 75],
     minimumCacheTTL: 5184000,
   },
 
